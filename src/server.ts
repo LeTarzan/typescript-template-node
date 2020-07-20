a3fb0ef7cdada2f3adbe34aa00cd4f351f61b489
LeTarzan <leandrorevolve@gmail.com>
2020-07-19 21:48:03 -0300

    Configure structure

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' })
 })
 
 app.listen(3333)
